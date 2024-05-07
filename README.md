5455e3154193e5bedb06b8d9aa077f1608b97aa8
alfian-f <alfian.0615@gmail.com>
2024-05-07 19:27:23 +0700

    Experiment 1.3: Multiple Spawn and removing drop

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ### [1.2]
 ![1.2 image](assets/1.2.png)
 The reason "Alfian's Computer: hey hey!" is printed first is that it is outside the asynchronous task and executes immediately, while the asynchronous task runs concurrently in the background.
+
+### [1.3] Multiple Spawner
+![multiple spawner output](<assets/1.3-multiple spawner.png>)
+
+### [1.3] Removed drop(spawner)
+![without drop output](<assets/1.3-removed drop.png>)
--- src/main.rs
+++ src/main.rs
@@ -106,6 +106,20 @@ fn main() {
         TimerFuture::new(Duration::new(2, 0)).await;
         println!("Alfian's Computer: done!");
     });
+    
+    spawner.spawn(async {
+        println!("Alfian's Computer: howdy2!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Alfian's Computer: done2!");
+    });
+
+    spawner.spawn(async {
+        println!("Alfian's Computer: howdy3!");
+        // Wait for our timer future to complete after two seconds.
+        TimerFuture::new(Duration::new(2, 0)).await;
+        println!("Alfian's Computer: done3!");
+    });
 
     println!("Alfian's Computer: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
