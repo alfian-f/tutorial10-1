da96cb663b49a8d2f595453effaf4088a755ba5c
alfian-f <alfian.0615@gmail.com>
2024-05-07 10:00:42 +0700

    Experiment 1.2: Understanding how it works.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reflections
+
+### [1.2]
+![1.2 image](assets/1.2.png)
+The reason "Alfian's Computer: hey hey!" is printed first is that it is outside the asynchronous task and executes immediately, while the asynchronous task runs concurrently in the background.
--- src/main.rs
+++ src/main.rs
@@ -107,6 +107,7 @@ fn main() {
         println!("Alfian's Computer: done!");
     });
 
+    println!("Alfian's Computer: hey hey!");
     // Drop the spawner so that our executor knows it is finished and won't
     // receive more incoming tasks to run.
     drop(spawner);
